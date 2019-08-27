52eb80efe7bdf4ce3deb97fd415f1cb3e053595e
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2019-08-27 22:36:18 +0300

    chore: update repository url

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ import os
 setup(
     name = "airsim_gym",
     version = "0.1",
+    packages = ['airsim_gym']
     author = "Konstantinos Kamaropoulos",
     author_email = "k@kamaropoulos.com",
     description = ("An OpenAI Gym environment for Microsoft's AirSim Multirotor simulator"),
     license = "MIT",
     keywords = "AirSim, OpenAI Gym, Gym, reinforcement learning, multirotor",
-    url = "http://packages.python.org/an_example_pypi_project",
-    install_requires=['gym', 'opencv-python', 'eventlet', 'airsim', 'matplotlib', 'Pillow'],
+    url = "https://github.com/Kamaropoulos/AirSim_Gym",
+    install_requires=['gym', 'opencv-python', 'eventlet', 'airsim', 'matplotlib', 'Pillow']
 )
